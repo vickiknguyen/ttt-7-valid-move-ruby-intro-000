12e729510b73f08179ce001853eb5047212d97b9
Vicki Kieu Nguyen <vickikieunguyen@gmail.com>
2020-11-02 21:44:33 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,8 @@
 # code your #valid_move? method here
 def valid_position?(board,index)
 
+end
+
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(board,index)
   index.to_i
