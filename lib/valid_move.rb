a26a89d6605da0409247c1ee0de7d44b62802d63
Vicki Kieu Nguyen <vickikieunguyen@gmail.com>
2020-11-02 21:47:30 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,8 +1,5 @@
 # code your #valid_move? method here
-def valid_position?(board,index)
- if valid_position(index.to_i) == [0,8]
-   true
-end
+
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(board,index)
